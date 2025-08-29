e63b24c9a56fae8026559bdfcbd6ac0e38e500ca
gongjaehyeok <ruud091@gmail.com>
2025-08-29 18:51:09 +0900

    try tryPromise

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Effect } from "effect";
 
-const fetchRequeset = Effect.promise(() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"));
-const jsonResponse = (response: Response) => Effect.promise(() => response.json());
+const fetchRequeset = Effect.tryPromise(() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"));
+const jsonResponse = (response: Response) => Effect.tryPromise(() => response.json());
 
 const main = Effect.flatMap(
   fetchRequeset,
