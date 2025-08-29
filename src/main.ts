22665962e907cabb3c5e12969fdc30c4dc429abc
gongjaehyeok <ruud091@gmail.com>
2025-08-29 19:32:56 +0900

    try pipe

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { Effect } from "effect";
 const fetchRequeset = Effect.tryPromise(() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"));
 const jsonResponse = (response: Response) => Effect.tryPromise(() => response.json());
 
-const main = Effect.flatMap(
-  fetchRequeset,
-  jsonResponse
-);
+const main = fetchRequeset.pipe(
+  Effect.flatMap(jsonResponse)
+)
 
 Effect.runPromise(main);
