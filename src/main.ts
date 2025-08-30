7d74b28910e9d5ef3e0249cf3ff25c9822b086f1
gongjaehyeok <ruud091@gmail.com>
2025-08-30 12:03:49 +0900

    try catchTag

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { Effect } from "effect";
 
-const fetchRequeset = Effect.tryPromise(() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"));
-const jsonResponse = (response: Response) => Effect.tryPromise(() => response.json());
+const fetchRequeset = Effect.tryPromise(() =>
+  fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
+);
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise(() => response.json());
 
 const main = fetchRequeset.pipe(
-  Effect.flatMap(jsonResponse)
-)
+  Effect.flatMap(jsonResponse),
+  Effect.catchTag("UnknownException", () =>
+    Effect.succeed("There was an error"),
+  ),
+);
 
-Effect.runPromise(main);
+Effect.runPromise(main).then(console.log);
