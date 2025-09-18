47e35e6e7d48821b2eaa72d4bc67d9ba425b94be
gongjaehyeok <ruud091@gmail.com>
2025-09-18 14:28:26 +0900

    try Context.tag

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -3,32 +3,33 @@ import { Pokemon } from "./schemas";
 import { FetchError, JsonError } from "./errors";
 import { ConfigError } from "effect/ConfigError";
 
-export interface PokeApi {
+export interface PokeApiImpl {
   readonly getPokemon: Effect.Effect<
     typeof Pokemon.Type,
     FetchError | JsonError | ParseResult.ParseError | ConfigError
   >;
 }
 
-export const PokeApi = Context.GenericTag<PokeApi>("PokeApi");
+export class PokeApi extends Context.Tag("PokeApi")<PokeApi, PokeApiImpl>(){
+  static readonly Live = PokeApi.of({
+    getPokemon: Effect.gen(function* () {
+      const baseUrl = yield* Config.string("BASE_URL");
+      const response = yield* Effect.tryPromise({
+        try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
+        catch: (): FetchError => new FetchError(),
+      });
 
-export const PokeApiLive = PokeApi.of({
-  getPokemon: Effect.gen(function* () {
-    const baseUrl = yield* Config.string("BASE_URL");
-    const response = yield* Effect.tryPromise({
-      try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
-      catch: (): FetchError => new FetchError(),
-    });
+      if (!response.ok) {
+        return yield* new FetchError();
+      }
 
-    if (!response.ok) {
-      return yield* new FetchError();
-    }
+      const json = yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: (): JsonError => new JsonError(),
+      });
 
-    const json = yield* Effect.tryPromise({
-      try: () => response.json(),
-      catch: (): JsonError => new JsonError(),
-    });
+      return yield* Schema.decode(Pokemon)(json);
+    }),
+  });
+};
 
-    return yield* Schema.decode(Pokemon)(json);
-  }),
-});
